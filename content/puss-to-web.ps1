c6c4a6d568937e46df2c84b47d7f2c58400a5c71
raf181 <93051606+raf181@users.noreply.github.com>
2024-10-14 19:34:09 +0200

    update

--- content/puss-to-web.ps1
+++ content/puss-to-web.ps1
@@ -7,11 +7,11 @@ $publish = Read-Host "Do you want to publish the latest version? (yes/no)"
 if ($publish -eq "yes") {
     # Execute npx quartz sync
     Write-Host "indexing files..."
-    Set-Location C:\Users\rafa\OneDrive\Documentos\GitHub\quartz\content\
+    Set-Location C:\Users\rafa\OneDrive\Documentos\GitHub\1-Ciberseguridad-web\content\
     python indexer.py
     Write-Host "Publishing latest version..."
-    Set-Location C:\Users\rafa\OneDrive\Documentos\GitHub\quartz\
-    npx quartz sync
+    Set-Location C:\Users\rafa\OneDrive\Documentos\GitHub\1-Ciberseguridad-web
+    npx quartz sync --no-pull
     git push
 } else {
     Write-Host "Publishing cancelled."
